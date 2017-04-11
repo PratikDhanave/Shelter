0710b4324bcfc6197f05f91becfdd8fd4204f173
PratikDhanave <i.pratikdhanave@gmail.com>
2017-04-11 16:40:08 +0530

    conflcit Resolved

--- scripts/test_download_image.py
+++
@@ -1,7 +0,0 @@
-url= "http://45.56.104.240"
-req = urllib2.Request(url)
-req.add_header('Authorization', 'OAuth2 a0028f740988d80cbe670f24a9456d655b8dd419')
-resp = urllib2.urlopen(req)
-content = resp.read()
-data = json.loads(content)
-result = data
